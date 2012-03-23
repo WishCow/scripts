7ccc3fc32a126f0671d45561d44dee53d13631c5
Norbert Kéri <wishcow@archer.localdomain>
2012-03-23 02:03:32 +0100

    Fix case when acpitool reports reports "Full"

--- tmux/status_right2.sh
+++ tmux/status_right2.sh
@@ -2,21 +2,19 @@
 
 case "$(hostname)" in
     archer)
-        ACPI=$(acpitool | head -n1)
-        PATTERN=": (.*?), ([0-9]+)\..*, (.*)"
+        ACPI=$(acpitool -b)
+        IFS=$', '
+        set -- ${ACPI#*:}
         echo -n "[BAT] "
-        if [[ $ACPI =~ $PATTERN ]]; then
-            case "${BASH_REMATCH[1]}" in
-                Charging)
-                    echo -n "${BASH_REMATCH[2]}%"
-                ;;
-                Discharging)
-                    echo -n "${BASH_REMATCH[3]} (${BASH_REMATCH[2]}%)"
-                ;;
-                Unknown)
-                    echo -n "100%"
-                ;;
-            esac
-        fi
-        ;;
+        case "$1" in
+            Charging)
+                echo -n "$2"
+            ;;
+            Discharging)
+                echo -n "$3 ($2)"
+            ;;
+            Unknown|Full)
+                echo -n "100%"
+            ;;
+    esac
 esac
