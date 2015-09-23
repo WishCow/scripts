e04de6a6c21403a4d498990275b7b4946e7af36c
Norbert Kéri <norbert.keri@pearson.com>
2015-09-23 12:10:13 +0200

    Parse out song and artist in youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -1,6 +1,27 @@
-#!/bin/bash
+#!/bin/bash -eu
+
+URL="$1"
+shift
+
+ARTIST="${1:-}"
+if [ -n "$ARTIST" ]; then
+    shift;
+    SONG="${1:-}"
+    if [ -n "$SONG" ]; then
+        shift;
+    fi
+fi
+
+ID3LINE="id3v2 -c Youtube"
+if [ -n "$ARTIST" ]; then
+    ID3LINE+=" -a '$ARTIST'"
+fi
+if [ -n "$SONG" ]; then
+    ID3LINE+=" -t '$SONG'"
+fi
+ID3LINE+=" {}"
 
 (
     cd ~/Downloads
-    youtube-dl -f bestaudio -x --audio-format mp3 "$@"
+    youtube-dl -f bestaudio -x --audio-format mp3 "$URL" "$@" --exec "$ID3LINE"
 )
