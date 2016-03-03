cc0e08fd246a608e9fb5423a9e28408ac9676361
Norbert Kéri <keri.norbert.miklos@gmail.com>
2016-03-04 00:40:04 +0100

    Make the sinks and brightness scripts a bit more robust

--- peripherials/brightness.py
+++ peripherials/brightness.py
@@ -5,7 +5,7 @@ from subprocess import call, check_output
 from os.path import basename
 from os import getenv
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     sys.stderr.write("Usage: {} number|display".format(basename(__file__)))
     sys.exit(1)
 
@@ -13,14 +13,8 @@ icon = "info"
 notification_id = 9999
 dunstify = getenv('HOME') + "/bin/dunstify"
 
-try:
-    delta = sys.argv[1]
-    current = int(round(float(check_output('xbacklight')), 0))
-    new_value = current
-    if delta != "display":
-        new_value = min(100, current + int(delta))
-        call(["xbacklight", "-inc", str(delta)])
-    call([dunstify, "-t", "3", "-r", str(notification_id), "Brightness", "-i", icon, "{} / 100".format(new_value)])
-except ValueError:
-    sys.stderr.write("{} is not a number".format(sys.argv[1]))
-    sys.exit(1)
+change, amount = sys.argv[1:]
+args = ["light", '-A' if change == 'inc' else '-U', amount]
+call(args)
+new_value = float(check_output(['light']))
+call([dunstify, "-t", "3", "-r", str(notification_id), "Brightness", "-i", icon, "{} / 100".format(new_value)])
--- sinks.py
+++ sinks.py
@@ -1,39 +1,36 @@
 #!/bin/python
 
-from subprocess import call, check_output, Popen, PIPE
+from subprocess import call, check_output, Popen, PIPE, STDOUT
 from pprint import pprint
 import re, sys
-from collections import OrderedDict
+from operator import itemgetter
 
-mycall = lambda x: check_output(x).decode('utf-8').rstrip("\n").split("\n")
-dmenu = ['rofi', '-dmenu', '-format', 'i', '-p', 'Choose sink to migrate to: ']
+mycall = lambda x: check_output(x, stderr=STDOUT).decode('utf-8').rstrip("\n")
+dmenu = ['rofi', '-dmenu', '-p', 'Choose sink to migrate to: ']
 
-choices = OrderedDict()
-l_sink, c_id = True, None
+contents = mycall(['pactl', 'list', 'sinks'])
+matches = re.findall(r'^Sink #(\d+).*?Description: (.*?)$', contents, flags=re.M | re.DOTALL)
 
-for line in mycall(['pactl', 'list', 'sinks']):
-    line = line.strip()
-    if l_sink:
-        match = re.match(r'^Sink #(\d+)$', line)
-        if match:
-            l_sink, c_id = False, match.group(1)
-    else:
-        match = re.match(r'^Description: (.*)$', line)
-        if match:
-            choices[c_id] = "{}. {}".format(c_id, match.group(1))
-            l_sink, c_id = True, None
-
-if not choices:
+if not matches:
+    print("No sinks found", file=sys.stderr)
     sys.exit(2)
 
+sinks = { "{}. {}".format(k,v): k for (k, v) in matches }
+
 pipe = Popen(dmenu, stdout=PIPE, stdin=PIPE)
-out, err = pipe.communicate(input="\n".join(choices.values()).encode('utf-8'))
+out, err = pipe.communicate(input="\n".join(sorted(sinks.keys())).encode('utf-8'))
+
+if err:
+    print("Could not call rofi:", err, file=sys.stderr)
 
 choice = out.decode("utf-8").strip()
-if not choice or choice not in choices:
+sink_id = sinks.get(choice)
+if not sink_id:
+    valid_sinks = ", ".join(sorted(sinks.keys()))
+    print("Selected choice {} is not a valid sink (valid sinks are: {})".format(choice, valid_sinks), file=sys.stderr)
     sys.exit(1)
 
-call(['pactl', 'set-default-sink', choice])
-for input in mycall(['pactl', 'list', 'short', 'sink-inputs']):
+call(['pactl', 'set-default-sink', sink_id], stderr=STDOUT)
+for input in mycall(['pactl', 'list', 'short', 'sink-inputs']).split("\n"):
     id = input.split("\t")[0]
-    call(['pactl', 'move-sink-input', id, choice])
+    call(['pactl', 'move-sink-input', id, sink_id])
