285144ba7e318ea9ca32675def2f37762f52b987
Norbert Kéri <keri.norbert.miklos@gmail.com>
2016-08-08 23:48:46 +0200

    Keyboard wip

--- keyboard.sh
+++ keyboard.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-setxkbmap -layout hu,no,us -option grp:alt_shift_toggle,caps:swapescape
-xmodmap ~/.xmodmap
+if [ "$1" == "ergo" ]; then
+    setxkbmap -layout hu,no,us -option grp:alt_shift_toggle,caps:escape
+    xmodmap ~/.xmodmap.ergo
+else
+    setxkbmap -layout hu,no,us -option grp:alt_shift_toggle
+    xmodmap ~/.xmodmap
+fi
+
 killall xcape
-xcape -e Control_L=Escape -t 110
+xcape -e 'Control_L=Escape' -t 110
+xcape -e 'Shift_L=parenleft;Shift_R=parenright;Alt_L=Shift_L|1'
