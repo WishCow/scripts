f2f3189220a3c98665c2bcc17fc322d5b3eb1702
Norbert Kéri <keri.norbert.miklos@gmail.com>
2016-11-24 19:57:55 +0100

    Allow tuning volume levels

--- blocklets/volume.sh
+++ blocklets/volume.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+if [[ -n "$BLOCK_BUTTON" ]]; then
+    cmd=""
+    case "$BLOCK_BUTTON" in
+        2)
+            cmd="toggle"
+            ;;
+        4)
+            cmd="10"
+            ;;
+        5)
+            cmd="-10"
+            ;;
+        *)
+            echo "$BLOCK_BUTTON"
+            ;;
+    esac
+    ~/bin/peripherials/volume.py "$cmd" > /dev/null
+fi
+
 if ponymix is-muted; then
     echo "  OFF"
     exit 0
